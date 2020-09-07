d4622a314fc1b3c71257dd75b6fcad8e0e19dc1b
Ben Branyon <benbranyon@gmail.com>
2020-09-06 19:15:53 -0700

    add some initial markup for aos prototype

--- wp-content/themes/hueman-child-theme/functions.php
+++ wp-content/themes/hueman-child-theme/functions.php
@@ -2,6 +2,7 @@
 /* Write your awesome functions below */
 
 function custom_scripts() {
+	wp_enqueue_style( 'aos-styles', get_stylesheet_directory_uri() . '/css/aos.css' );
     wp_enqueue_script('custom-script', get_stylesheet_directory_uri() . '/js/custom-scripts.js', array( 'jquery' ));
 }
-add_action( 'wp_enqueue_scripts', 'custom_scripts' );
+add_action( 'wp_enqueue_scripts', 'custom_scripts', 99 );
--- wp-content/themes/hueman-child-theme/page-architecture-of-surveillance.php
+++ wp-content/themes/hueman-child-theme/page-architecture-of-surveillance.php
@@ -8,6 +8,24 @@ get_header(); ?>
 
 <div class="content">
 	<h1><?php the_title(); ?></h1>
+	<div class="aos-intro-image">
+		<img src="/wp-content/themes/hueman-child-theme/assets/images/aos-still.png"/>
+	</div>
+	<div class="aos-infographic">
+		<div class="aos-infographic__info">
+			<h2>Trapwire</h2>
+			<ul>
+				<li>Street camera style surveillance system– more accurate than modern facial recognition technology</li>
+				<li>Every few seconds, data picked up at surveillance points are recorded digitally on the spot, then encrypted and instantaneously delivered to a fortified central database center at an undisclosed location to be aggregated with other intelligence.</li>
+				<li>Documents have revealed that LAPD has invested heavily in this.</li>
+			</ul>
+		</div>
+		<div class="aos-infographic__animation">
+		    <video autoplay="" class="hero-video" muted="" playsinline="" poster="/wp-content/themes/hueman-child-theme/assets/images/aos-still.png">
+		      <source src="/wp-content/themes/hueman-child-theme/assets/videos/2nd-rising.mp4" type="video/mp4" />
+		    </video>
+		</div>
+	</div>
 </div>
 
 <?php get_footer(); ?>
