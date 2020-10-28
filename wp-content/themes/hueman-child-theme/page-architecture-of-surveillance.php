5c7afd1c452a184e360d785314c7a6d27682a761
Ben Branyon <benbranyon@gmail.com>
2020-10-28 16:21:49 -0700

    more basic styling

--- wp-content/themes/hueman-child-theme/page-architecture-of-surveillance.php
+++ wp-content/themes/hueman-child-theme/page-architecture-of-surveillance.php
@@ -7,14 +7,14 @@ Template Post Type: page
 get_header(); ?>
 
 <div class="content">
-	<h1><?php the_title(); ?></h1>
+	<h1 class="aos-page-title"><?php the_title(); ?></h1>
 	<div class="aos-infographic">
 		<div class="aos-infographic__info">
 			<div class="aos-infographic__display">
 			</div>
 			<div class="aos-infographic__text" id="infographic__text1">
-				<h2>Joint Terrorism Task Force</h2>
-				<ul>
+				<h2 class="aos-infographic__title">Joint Terrorism Task Force</h2>
+				<ul class="aos-list">
 					<li>The FBI conducts its counter-terrorism intelligence operations primarily through Joint Terrorism Task Forces (JTTF).</li>
 					<li>According to the FBI’s website, over 600 state, local and federal agencies participate in JTTFs, including the U.S. military and, at one point at least, the Central Intelligence Agency (CIA).</li>
 					<li>In 2004 the ACLU engaged in a FOIA campaign that uncovered FBI JTTF spying on political advocacy organizations.</li>
@@ -22,8 +22,8 @@ get_header(); ?>
 				</ul>
 			</div>
 			<div class="aos-infographic__text" id="infographic__text2">
-				<h2>Trapwire</h2>
-				<ul>
+				<h2 class="aos-infographic__title">Trapwire</h2>
+				<ul class="aos-list">
 					<li>Street camera style surveillance system– more accurate than modern facial recognition technology</li>
 					<li>Every few seconds, data picked up at surveillance points are recorded digitally on the spot, then encrypted and instantaneously delivered to a fortified central database center at an undisclosed location to be aggregated with other intelligence.</li>
 					<li>Documents have revealed that LAPD has invested heavily in this.</li>
