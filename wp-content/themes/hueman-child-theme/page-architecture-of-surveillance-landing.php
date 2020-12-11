2366be57d030cab37a945c2edab0015df24983e6
Ben Branyon <benbranyon@gmail.com>
2020-12-10 18:23:00 -0800

    add initial landing page

---
+++ wp-content/themes/hueman-child-theme/page-architecture-of-surveillance-landing.php
@@ -0,0 +1,24 @@
+<?php 
+/*
+Template Name: Architecture of Surveillance Landing
+Template Post Type: page
+*/
+
+get_header(); ?>
+
+<div class="content">
+	<div class="aos-landing">
+		<h1 class="aos-landing-title"><img src="/wp-content/themes/hueman-child-theme/assets/images/AoS_Title.png" alt="Architecture of Surveillance"/></h1>
+		<div class="aos-landing__description">
+			<p>
+				Large amounts of information gathering by police departments is done through electronic surveillance equipment such as Stingrays, automatic license plate readers (ALPR), TrapWire, high definition cameras, body cameras, drones, CCTV, social media surveillance, and much more. Information collected by these surveillance tools criminalize a person’s daily lived experience. Documents obtained by the Stop LAPD Spying Coalition have repeatedly demonstrated that these programs inflict grave harm to community members. Without the massive amounts of surveillance equipment deployed by LAPD, creating ever-present surveillance within communities, the operationalization of programs like Data Informed Community Focused Policing could not exist. The now defunct LASER program is an example of how location-based policing and person-based policing was dependent on CCTV, ALPRs, risk assessments, crime databases, Field Interview (FI) cards, and ArcGIS, with plans to expand the use of Palantir mobile and social network analysis into the program.
+			</p>
+		</div>
+		<div class="aos-landing__nav">
+			<a href="#" class="button">Interactive</a>
+			<a href="/architecture-of-surveillance/" class="button">Immersive</a>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
